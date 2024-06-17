f74095a6c2c36364789bbb5a54c9cae6aa18d464
Noemi Spurio <n.spurio@solvingteam.it>
2024-06-17 10:18:22 +0200

    Initial commit

---
+++ src/main/java/com/nhy/mtgeenie/MtgeenieApplication.java
@@ -0,0 +1,13 @@
+package com.nhy.mtgeenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgeenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgeenieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhy/mtgeenie/MtgeenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhy.mtgeenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgeenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
