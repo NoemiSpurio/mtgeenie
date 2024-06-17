b63718ee84a6f416c50cc15550291c5043b52a5d
Noemi Spurio <n.spurio@solvingteam.it>
2024-06-17 15:43:08 +0200

    script to insert sets - part 1

---
+++ src/main/resources/db/changelog/include/script/insert-sets.sql
@@ -0,0 +1,190 @@
+INSERT INTO set (id, name, code, type, notes, release_date)
+VALUES (UUID(), 'Alpha (Limited Edition)', 'LEA', 'Core set', 'Core set #1 (1st printing)', '1993-08-05'),
+VALUES (UUID(), 'Beta (Limited Edition)', 'LEB', 'Core set', 'Core set #1 (2nd printing)', '1993-10-04'),
+VALUES (UUID(), 'Unlimited Edition', '2ED', 'Core set', 'Core set #2', '1993-12-01'),
+VALUES (UUID(), 'Arabian Nights', 'ARN', 'Expansion set', '1st expansion', '1993-12-17'),
+VALUES (UUID(), 'Antiquities', 'ATQ', 'Expansion set', '2nd expansion', '1994-03-04'),
+VALUES (UUID(), 'Revised Edition', '3ED', 'Core set', 'Core set #3', '1994-04-11'),
+VALUES (UUID(), 'Legends', 'LEG', 'Expansion set', '3rd expansion', '1994-06-10'),
+VALUES (UUID(), 'The Dark', 'DRK', 'Expansion set', '4th expansion', '1994-08-08'),
+VALUES (UUID(), 'Fallen Empires', 'FEM', 'Expansion set', '5th expansion', '1994-11-15'),
+VALUES (UUID(), 'Fourth Edition', '4ED', 'Core set', 'Core set #4', '1995-05-03'),
+VALUES (UUID(), 'Ice Age', 'ICE', 'Expansion set', '6th expansion, 1st standalone set', '1995-06-05'),
+VALUES (UUID(), 'Chronicles', 'CHR', 'Compilation set', 'Expansion symbol from original printing', '1995-07-01'),
+VALUES (UUID(), 'Renaissance', null, 'Compilation set', null, '1995-08-01'),
+VALUES (UUID(), 'Homelands', 'HML', 'Expansion set', '7th expansion', '1995-10-14'),
+VALUES (UUID(), 'Alliances', 'ALL', 'Expansion set', '8th expansion', '1996-06-10'),
+VALUES (UUID(), 'Mirage', 'MIR', 'Expansion set', '9th expansion, 2nd standalone set', '1996-10-08'),
+VALUES (UUID(), 'Visions', 'VIS', 'Expansion set', '10th expansion', '1997-02-03'),
+VALUES (UUID(), 'Fifth Edition', '5ED', 'Core set', 'Core set #5', '1997-03-24'),
+VALUES (UUID(), 'Portal', 'POR', 'Starter set', 'Starter set #1', '1997-05-01'),
+VALUES (UUID(), 'Weatherlight', 'WTH', 'Expansion set', '11th expansion', '1997-06-09'),
+VALUES (UUID(), 'Tempest', 'TMP', 'Expansion set', '12th expansion, 3rd standalone set', '1997-10-14'),
+VALUES (UUID(), 'Stronghold', 'STH', 'Expansion set', '13th expansion', '1998-03-02'),
+VALUES (UUID(), 'Exodus', 'EXO', 'Expansion set', '14th expansion', '1998-06-15'),
+VALUES (UUID(), 'Portal Second Age', 'P02', 'Starter set', 'Starter set #2', '1998-06-24'),
+VALUES (UUID(), 'Unglued', 'UGL', 'Un-set', 'Un-set #1', '1998-08-11'),
+VALUES (UUID(), 'Urza''s Saga', 'USG', 'Expansion set', '15th expansion', '1998-10-12'),
+VALUES (UUID(), 'Anthologies', 'ATH', 'Box set', 'First box set', '1998-11-01'),
+VALUES (UUID(), 'Urza''s Legacy', 'ULG', 'Expansion set', '16th expansion', '1999-02-15'),
+VALUES (UUID(), 'Sixth Edition', '6ED', 'Core set', 'Core set #6', '1999-04-21'),
+VALUES (UUID(), 'Portal Three Kingdoms', 'PTK', 'Starter set', 'Starter set #3', '1999-05-01'),
+VALUES (UUID(), 'Urza''s Destiny', 'UDS', 'Expansion set', '17th expansion', '1999-06-07'),
+VALUES (UUID(), 'Starter 1999', 'S99', 'Starter set', 'Starter set #4', '1999-07-12'),
+VALUES (UUID(), 'Mercadian Masques', 'MMQ', 'Expansion set', '18th expansion', '1999-10-04'),
+VALUES (UUID(), 'Battle Royale', 'BRB', 'Box set', null, '1999-11-12'),
+VALUES (UUID(), 'Nemesis', 'NEM', 'Expansion set', '19th expansion', '2000-02-14'),
+VALUES (UUID(), 'Starter 2000', 'S00', 'Starter set', 'Starter set #5', '2000-04-24'),
+VALUES (UUID(), 'Prophecy', 'PCY', 'Expansion set', '20th expansion', '2000-06-05'),
+VALUES (UUID(), 'Invasion', 'INV', 'Expansion set', '21st expansion', '2000-10-02'),
+VALUES (UUID(), 'Beatdown', 'BTD', 'Box set', null, '2000-10-01'),
+VALUES (UUID(), 'Planeshift', 'PLS', 'Expansion set', '22nd expansion', '2001-02-05'),
+VALUES (UUID(), 'Seventh Edition', '7ED', 'Core set', 'Core set #7', '2001-04-11'),
+VALUES (UUID(), 'Apocalypse', 'APC', 'Expansion set', '23rd expansion', '2001-06-04'),
+VALUES (UUID(), 'Odyssey', 'ODY', 'Expansion set', '24th expansion', '2001-10-01'),
+VALUES (UUID(), 'Deckmasters 2001', 'DKM', 'Box set', null, '2001-12-01'),
+VALUES (UUID(), 'Torment', 'TOR', 'Expansion set', '25th expansion', '2002-02-08'),
+VALUES (UUID(), 'Judgment', 'JUD', 'Expansion set', '26th expansion', '2002-05-27'),
+VALUES (UUID(), 'Onslaught', 'ONS', 'Expansion set', '27th expansion', '2002-10-07'),
+VALUES (UUID(), 'Legions', 'LGN', 'Expansion set', '28th expansion', '2003-02-03'),
+VALUES (UUID(), 'Scourge', 'SCG', 'Expansion set', '29th expansion', '2003-05-26'),
+VALUES (UUID(), 'Eighth Edition', '8ED', 'Core set', 'Core set #8', '2003-07-28'),
+VALUES (UUID(), 'Mirrodin', 'MRD', 'Expansion set', '30th expansion', '2003-10-02'),
+VALUES (UUID(), 'Darksteel', 'DST', 'Expansion set', '31st expansion', '2004-02-06'),
+VALUES (UUID(), 'Fifth Dawn', '5DN', 'Expansion set', '32nd expansion', '2004-06-04'),
+VALUES (UUID(), 'Champions of Kamigawa', 'CHK', 'Expansion set', '33rd expansion', '2004-10-01'),
+VALUES (UUID(), 'Unhinged', 'UNH', 'Un-set', 'Un-set #2', '2004-11-20'),
+VALUES (UUID(), 'Betrayers of Kamigawa', 'BOK', 'Expansion set', '34th expansion', '2005-02-04'),
+VALUES (UUID(), 'Saviors of Kamigawa', 'SOK', 'Expansion set', '35th expansion', '2005-06-03'),
+VALUES (UUID(), 'Ninth Edition', '9ED', 'Core set', 'Core set #9', '2005-07-29'),
+VALUES (UUID(), 'Salvat 2005', null, 'Compilation set', 'Magic Encyclopedia set #1', '2005-07-01'),
+VALUES (UUID(), 'Ravnica: City of Guilds', 'RAV', 'Expansion set', '36th expansion', '2005-10-07'),
+VALUES (UUID(), 'Guildpact', 'GPT', 'Expansion set', '37th expansion', '2006-02-03'),
+VALUES (UUID(), 'Dissension', 'DIS', 'Expansion set', '38th expansion', '2006-05-05'),
+VALUES (UUID(), 'Coldsnap', 'CSP', 'Expansion set', '39th expansion', '2006-07-21'),
+VALUES (UUID(), 'Time Spiral', 'TSP', 'Expansion set', '40th expansion', '2006-10-06'),
+VALUES (UUID(), 'Time Spiral Timeshifted', 'TSP', 'Compilation set', 'Bonus sheet set', '2006-10-06'),
+VALUES (UUID(), 'Planar Chaos', 'PLC', 'Expansion set', '41st expansion', '2007-02-02'),
+VALUES (UUID(), 'Future Sight', 'FUT', 'Expansion set', '42nd expansion', '2007-05-04'),
+VALUES (UUID(), 'Tenth Edition', '10E', 'Core set', 'Core set #10', '2007-07-13'),
+VALUES (UUID(), 'Masters Edition', 'MED', 'Compilation set', 'Magic Online Masters set #1', '2007-09-10'),
+VALUES (UUID(), 'Lorwyn', 'LRW', 'Expansion set', '43rd expansion', '2007-10-12'),
+VALUES (UUID(), 'Duel Decks: Elves vs. Goblins', 'EVG', 'Box set', 'Duel Decks set #1', '2007-11-16'),
+VALUES (UUID(), 'Morningtide', 'MOR', 'Expansion set', '44th expansion', '2008-02-01'),
+VALUES (UUID(), 'Shadowmoor', 'SHM', 'Expansion set', '45th expansion', '2008-05-02'),
+VALUES (UUID(), 'Eventide', 'EVE', 'Expansion set', '46th expansion', '2008-07-25'),
+VALUES (UUID(), 'From the Vault: Dragons', 'DRB', 'Box set', 'From the Vault set #1', '2008-08-29'),
+VALUES (UUID(), 'Masters Edition II', 'ME2', 'Compilation set', 'Magic Online Masters set #2', '2008-09-22'),
+VALUES (UUID(), 'Shards of Alara', 'ALA', 'Expansion set', '47th expansion', '2008-10-03'),
+VALUES (UUID(), 'Duel Decks: Jace vs. Chandra', 'DD2', 'Box set', 'Duel Decks set #2', '2008-11-07'),
+VALUES (UUID(), 'Alara Reborn', 'ARB', 'Expansion set', '49th expansion', '2009-04-30'),
+VALUES (UUID(), 'Magic 2010', 'M10', 'Core set', 'Core set #11', '2009-07-17'),
+VALUES (UUID(), 'Commander Theme Decks', 'TD0', 'Box set', 'Magic Online only', '2009-08-31'),
+VALUES (UUID(), 'From the Vault: Exiled', 'V09', 'Box set', 'From the Vault set #2', '2009-08-28'),
+VALUES (UUID(), 'Planechase', 'HOP', 'Supplemental set', 'Planechase set #1', '2009-09-04'),
+VALUES (UUID(), 'Masters Edition III', 'ME3', 'Compilation set', 'Magic Online Masters set #3', '2009-09-07'),
+VALUES (UUID(), 'Zendikar', 'ZEN', 'Expansion set', '50th expansion', '2009-10-02'),
+VALUES (UUID(), 'Duel Decks: Garruk vs. Liliana', 'DDD', 'Box set', 'Duel Decks set #4', '2009-10-30'),
+VALUES (UUID(), 'Premium Deck Series: Slivers', 'H09', 'Box set', 'Premium Deck Series set #1', '2009-11-20'),
+VALUES (UUID(), 'Worldwake', 'WWK', 'Expansion set', '51st expansion', '2010-02-05'),
+VALUES (UUID(), 'Duel Decks: Phyrexia vs. The Coalition', 'DDE', 'Box set', 'Duel Decks set #5', '2010-03-19'),
+VALUES (UUID(), 'Rise of the Eldrazi', 'ROE', 'Expansion set', '52nd expansion', '2010-04-23'),
+VALUES (UUID(), 'Deck Builder''s Toolkit', null, 'Box set', 'Deck Builder''s Toolkit #1', '2010-05-21'),
+VALUES (UUID(), 'Duels of the Planeswalkers', 'DPA', 'Box set', 'Inspired by the digital game', '2010-06-04'),
+VALUES (UUID(), 'Archenemy', 'ARC', 'Supplemental set', 'Archenemy set #1', '2010-06-18'),
+VALUES (UUID(), 'Magic 2011', 'M11', 'Core set', 'Core set #12', '2010-07-16'),
+VALUES (UUID(), 'From the Vault: Relics', 'V10', 'Box set', 'From the Vault set #3', '2010-08-27'),
+VALUES (UUID(), 'Duel Decks: Elspeth vs. Tezzeret', 'DDF', 'Box set', 'Duel Decks set #6', '2010-09-03'),
+VALUES (UUID(), 'Scars of Mirrodin', 'SOM', 'Expansion set', '53rd expansion', '2010-10-01'),
+VALUES (UUID(), 'Magic Online Deck Series', 'TD0', 'Box set', 'Magic Online only', '2010-11-08'),
+VALUES (UUID(), 'Premium Deck Series: Fire & Lightning', 'PD2', 'Box set', 'Premium Deck Series set #2', '2010-11-19'),
+VALUES (UUID(), 'Momir Basic Event Deck', null, 'Box set', 'Magic Online only', '2010-11-22'),
+VALUES (UUID(), 'Salvat 2011', null, 'Compilation set', 'Magic Encyclopedia set #2', '2011-01-01'),
+VALUES (UUID(), 'Masters Edition IV', 'ME4', 'Compilation set', 'Magic Online Masters set #4', '2011-01-10'),
+VALUES (UUID(), 'Mirrodin Besieged', 'MBS', 'Expansion set', '54th expansion', '2011-02-04'),
+VALUES (UUID(), 'Deck Builder''s Toolkit 2011', null, 'Box set', 'Deck Builder''s Toolkit #2. Last self-contained toolkit.', '2011-03-11'),
+VALUES (UUID(), 'Duel Decks: Knights vs. Dragons', 'DDG', 'Box set', 'Duel Decks set #7', '2011-04-01'),
+VALUES (UUID(), 'New Phyrexia', 'NPH', 'Expansion set', '55th expansion', '2011-05-13'),
+VALUES (UUID(), 'Commander', 'CMD', 'Box set', 'Commander set #1', '2011-06-17'),
+VALUES (UUID(), 'Magic 2012', 'M12', 'Core set', 'Core set #13', '2011-07-15'),
+VALUES (UUID(), 'From the Vault: Legends', 'V11', 'Box set', 'From the Vault set #4', '2011-08-26'),
+VALUES (UUID(), 'Duel Decks: Ajani vs. Nicol Bolas', 'DDH', 'Box set', 'Duel Decks set #8', '2011-09-02'),
+VALUES (UUID(), 'Innistrad', 'ISD', 'Expansion set', '56th expansion', '2011-09-30'),
+VALUES (UUID(), 'Premium Deck Series: Graveborn', 'PD3', 'Box set', 'Premium Deck Series set #3', '2011-11-18'),
+VALUES (UUID(), 'Dark Ascension', 'DKA', 'Expansion set', '57th expansion', '2012-02-03'),
+VALUES (UUID(), 'Duel Decks: Venser vs. Koth', 'DDI', 'Box set', 'Duel Decks set #9', '2012-03-30'),
+VALUES (UUID(), 'Avacyn Restored', 'AVR', 'Expansion set', '58th expansion', '2012-05-04'),
+VALUES (UUID(), 'Planechase 2012', 'PC2', 'Supplemental set', 'Planechase set #2', '2012-06-01'),
+VALUES (UUID(), 'Magic 2013', 'M13', 'Core set', 'Core set #14', '2012-07-13'),
+VALUES (UUID(), 'From the Vault: Realms', 'V12', 'Box set', 'From the Vault set #5', '2012-08-31'),
+VALUES (UUID(), 'Duel Decks: Izzet vs. Golgari', 'DDJ', 'Box set', 'Duel Decks set #10', '2012-09-07'),
+VALUES (UUID(), 'Return to Ravnica', 'RTR', 'Expansion set', '59th expansion', '2012-10-05'),
+VALUES (UUID(), 'Commander''s Arsenal', 'CM1', 'Box set', 'Commander set #2', '2012-11-02'),
+VALUES (UUID(), 'Duel Decks: Mirrodin Pure vs. New Phyrexia', 'TD2', 'Box set', 'Magic Online Duel Deck', '2013-01-11'),
+VALUES (UUID(), 'Gatecrash', 'GTC', 'Expansion set', '60th expansion', '2013-02-01'),
+VALUES (UUID(), 'Duel Decks: Sorin vs. Tibalt', 'DDK', 'Box set', 'Duel Decks set #11', '2013-03-15'),
+VALUES (UUID(), 'Dragon''s Maze', 'DGM', 'Expansion set', '61st expansion', '2013-05-03'),
+VALUES (UUID(), 'Modern Masters', 'MMA', 'Compilation set', 'Masters set #1', '2013-06-07'),
+VALUES (UUID(), 'Magic 2014', 'M14', 'Core set', 'Core set #15', '2013-07-19'),
+VALUES (UUID(), 'From the Vault: Twenty', 'V13', 'Box set', 'From the Vault set #6', '2013-08-23'),
+VALUES (UUID(), 'Duel Decks: Heroes vs. Monsters', 'DDL', 'Box set', 'Duel Decks set #12', '2013-09-06'),
+VALUES (UUID(), 'Theros', 'THS', 'Expansion set', '62nd expansion', '2013-09-27'),
+VALUES (UUID(), 'Commander 2013', 'C13', 'Box set', 'Commander set #3', '2013-11-01'),
+VALUES (UUID(), 'Born of the Gods', 'BNG', 'Expansion set', '63rd expansion', '2014-02-07'),
+VALUES (UUID(), 'Duel Decks: Jace vs. Vraska', 'DDM', 'Box set', 'Duel Decks set #13', '2014-03-14'),
+VALUES (UUID(), 'Journey into Nyx', 'JOU', 'Expansion set', '64th expansion', '2014-05-02'),
+VALUES (UUID(), 'Modern Event Deck', 'MD1', 'Box set', null, '2014-05-30'),
+VALUES (UUID(), 'Conspiracy', 'CNS', 'Supplemental set', 'Conspiracy set #1', '2014-06-06'),
+VALUES (UUID(), 'Vintage Masters', 'VMA', 'Compilation set', 'Magic Online Masters set #5', '2014-06-16'),
+VALUES (UUID(), 'Magic 2015', 'M15', 'Core set', 'Core set #16', '2014-07-18'),
+VALUES (UUID(), 'From the Vault: Annihilation', 'V14', 'Box set', 'From the Vault set #7', '2014-08-22'),
+VALUES (UUID(), 'Duel Decks: Speed vs. Cunning', 'DDN', 'Box set', 'Duel Decks set #14', '2014-09-05'),
+VALUES (UUID(), 'Khans of Tarkir', 'KTK', 'Expansion set', '65th expansion, MTG Arena set #6', '2014-09-26'),
+VALUES (UUID(), 'Commander 2014', 'C14', 'Box set', 'Commander set #4', '2014-11-07'),
+VALUES (UUID(), 'Duel Decks Anthology', 'DD3', 'Box set', 'Duel Decks reprint set', '2014-12-05'),
+VALUES (UUID(), 'Fate Reforged', 'FRF', 'Expansion set', '66th expansion', '2015-01-23'),
+VALUES (UUID(), 'Duel Decks: Elspeth vs. Kiora', 'DDO', 'Box set', 'Duel Decks set #15', '2015-02-27'),
+VALUES (UUID(), 'Dragons of Tarkir', 'DTK', 'Expansion set', '67th expansion', '2015-03-27'),
+-- VALUES (UUID(), 'Tempest Remastered', 'DTK', 'Expansion set', '67th expansion', '2015-03-27'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
