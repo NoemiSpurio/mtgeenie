4d7b7a6d133a5b34dee001b797e1b254239a6d9d
Noemi Spurio <n.spurio@solvingteam.it>
2024-06-19 16:44:46 +0200

    script to insert card types

---
+++ src/main/resources/db/changelog/include/script/insert-types.sql
@@ -0,0 +1,17 @@
+INSERT INTO type (id, name)
+VALUES (UUID(), 'ARTIFACT'),
+       (UUID(), 'BATTLE'),
+       (UUID(), 'CONSPIRACY'),
+       (UUID(), 'CREATURE'),
+       (UUID(), 'DUNGEON'),
+       (UUID(), 'ENCHANTMENT'),
+       (UUID(), 'INSTANT'),
+       (UUID(), 'KINDRED'),
+       (UUID(), 'LAND'),
+       (UUID(), 'PHENOMENON'),
+       (UUID(), 'PLANE'),
+       (UUID(), 'PLANESWALKER'),
+       (UUID(), 'SCHEME'),
+       (UUID(), 'SORCERY'),
+       (UUID(), 'VANGUARD');
+
