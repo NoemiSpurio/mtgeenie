fbabc14f78f83383c675b44770278b0e4892e50b
Noemi Spurio <n.spurio@solvingteam.it>
2024-07-08 14:55:16 +0200

    security config

---
+++ src/main/java/com/nhy/mtgeenie/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.nhy.mtgeenie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/*"))
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
